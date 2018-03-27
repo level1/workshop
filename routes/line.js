8751b1b14bdbec7c0e2b0030f6bfcc40782cf033
DEV1\Apichart <nuchbuti@gmail.com>
2018-03-27 17:56:18 +0700

    แก้ไข  config ที่ line.js

--- routes/line.js
+++ routes/line.js
@@ -6,7 +6,6 @@ var router = express.Router();
 const line = require('@line/bot-sdk')
 const linecontroller = require('../controllers/lineController');
 const configs = require('../config')
-const client = new line.Client(config);
 
 /* GET home page. */
 router.post('/webhook',line.middleware(configs.lineconfig), linecontroller.webhook);
